d01f5f860acffd440636dd54b2795e493b096d60
Esteban Alvarado <estalvgs1999@gmail.com>
2023-04-24 11:49:27 -0600

    [add] api key security

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const apiKeyMiddleware = require("../middlewares/api-key.handler");
 
 const missionRouter = require("./mission.routes");
 
 const routerApi = (app) => {
   const router = express.Router();
   app.use("/api/v1", router);
-  router.use("/mission", missionRouter);
+  router.use("/mission", apiKeyMiddleware, missionRouter);
 };
 
 module.exports = routerApi;
